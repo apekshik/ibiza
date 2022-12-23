6856b30ff444de9dafc2828d62c658de2de28919
Apekshik Panigrahi <46071272+apekshik@users.noreply.github.com>
2022-12-22 18:41:18 -0600

    Initial Commit

---
+++ ibiza/ibizaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ibizaApp.swift
+//  ibiza
+//
+//  Created by Apekshik Panigrahi on 12/22/22.
+//
+
+import SwiftUI
+
+@main
+struct ibizaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
